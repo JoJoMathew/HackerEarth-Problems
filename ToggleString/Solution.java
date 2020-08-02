e690b6b3ea437b4f12bd6fc77ca7ee3f0bf30495
Mathew Thomas <53473163+JoJoMathew@users.noreply.github.com>
2020-08-02 18:50:09 +0100

    Create Solution.java

---
+++ ToggleString/Solution.java
@@ -0,0 +1,28 @@
+//imports for BufferedReader
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//import for Scanner and other utility classes
+import java.util.*;
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+       
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();                // Reading input from STDIN
+
+        StringBuffer newStr=new StringBuffer(input);    
+
+         for(int i = 0; i < input.length(); i++) { 
+              
+            if(Character.isLowerCase(input.charAt(i))) {      
+                newStr.setCharAt(i, Character.toUpperCase(newStr.charAt(i)));  
+            } else if(Character.isUpperCase(input.charAt(i))) {       
+                newStr.setCharAt(i, Character.toLowerCase(newStr.charAt(i)));    
+            } 
+         }
+
+        System.out.println(newStr);    // Writing output to STDOUT
+    }
+}
